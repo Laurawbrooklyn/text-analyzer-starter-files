75acab1700088d2f3292cb62b98aa9dd27dc0570
Laura Wallace <lauranestwallace@gmail.com>
2017-05-05 11:02:36 -0400

    text analyzer app finished

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// your code here!
+function wordCount(text){
+  return(text.split(" ").length);
+}
+
+function uniqueWordCount(text) {
+  var arrayOfWords = (text.split(" "));
+  var uniqueWords = {};
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    uniqueWords[arrayOfWords[i]] = true;
+  }
+  return(Object.keys(uniqueWords).length);
+
+}
+
+function findTotalCharacters(text){
+  return(text.replace(/\s/g, '').length);
+  }
+function textAnalyzer(event) { 
+  var text = $("#user-text").val();
+  var average = findTotalCharacters(text) / wordCount(text);
+  event.preventDefault();
+$(".js-word-count").text(wordCount(text));
+$(".hidden").removeClass("hidden");
+$(".js-unique-word-count").text(uniqueWordCount(text));
+$(".js-average-word-length").text(average);
+}
+$("form").on("submit", textAnalyzer);
